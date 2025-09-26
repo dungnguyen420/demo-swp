edf4224096fad776c15b6e5c6e66c23f05c88bfd
Dung <attackgaming2004@gmail.com>
2025-09-27 01:11:01 +0700

    update BaseEntity

---
+++ src/main/java/com/example/swp/Entity/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.example.swp.Entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
+
