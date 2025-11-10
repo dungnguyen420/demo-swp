354aae5a4767cdb433cd355239d9e5135b6fe2e9
Vinh Đặng <quangvinh23072004@gmail.com>
2025-11-11 04:35:48 +0700

    Commit update vlaidation các Product

--- src/main/java/com/example/swp/DTO/ProductDTO.java
+++ src/main/java/com/example/swp/DTO/ProductDTO.java
@@ -1,13 +1,32 @@
 package com.example.swp.DTO;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Data;
 
 @Data
 public class ProductDTO {
     private long id;
+    @NotBlank(message = "Name cannot be blank")
+    @Pattern(
+            regexp = "^[A-Za-z0-9 ]+$",
+            message = "Name can only contain letters, numbers and spaces"
+    )
     private String name;
+    @NotBlank(message = "Description cannot be blank")
+    @Pattern(
+            regexp = "^[A-Za-z0-9 ]+$",
+            message = "Description can only contain letters, numbers and spaces"
+    )
     private String description;
+    @NotNull(message = "Price cannot be null")
+    @PositiveOrZero(message = "Price must be equal or greater than 0")
     private double price;
+    @NotNull(message = "Quantity cannot be null")
+    @PositiveOrZero(message = "Quantity must be equal or greater than 0")
     private int quantity;
+    @NotBlank(message = "Image cannot be blank")
     private String image;
 }
