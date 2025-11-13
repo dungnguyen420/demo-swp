9dc64b7111930c214b564c6ee6d283cdcb1abe45
Vinh Đặng <quangvinh23072004@gmail.com>
2025-11-14 02:18:53 +0700

    Commit update validation RegisterDTO

--- src/main/java/com/example/swp/DTO/RegisterDTO.java
+++ src/main/java/com/example/swp/DTO/RegisterDTO.java
@@ -1,15 +1,34 @@
 package com.example.swp.DTO;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class RegisterDTO {
+    @NotBlank(message = "Username không được để trống")
+    @Size(min = 3, max = 50, message = "Username 3-50 ký tự")
+    @Pattern(regexp = "^(?!\\s)(?!.*\\s$)(?!\\d+$)[\\p{L}\\p{N}\\-._]+$",
+            message = "Username chỉ gồm chữ và số * không toàn số, không cách")
     private String userName;
+    @NotBlank(message = "Mật khẩu không được để trống")
+    @Size(min = 8, max = 64, message = "Mật khẩu 8-64 ký tự")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[^\\s]{8,64}$",
+            message = "Mật khẩu phải có chữ, số và không có khoảng trắng")
     private String passWord;
+    @NotBlank(message = "Email không được để trống")
+    @Email(message = "Email không hợp lệ")
+    @Size(max = 150, message = "Email tối đa 150 ký tự")
     private String email;
+    @NotBlank(message = "Họ không được để trống")
+    @Size(max = 50, message = "Họ tối đa 50 ký tự")
+    @Pattern(regexp = "^(?!\\s)(?!.*\\s$)[\\p{L}]+(?:\\s[\\p{L}]+)*$",
+            message = "Họ chỉ gồm chữ, không cách đầu/cuối")
     private String firstName;
+    @NotBlank(message = "Tên không được để trống")
+    @Size(max = 50, message = "Tên tối đa 50 ký tự")
+    @Pattern(regexp = "^(?!\\s)(?!.*\\s$)[\\p{L}]+(?:\\s[\\p{L}]+)*$",
+            message = "Tên chỉ gồm chữ, không cách đầu/cuối")
     private String lastName;
+
     private String role;
 }
